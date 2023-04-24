22b33cc6e78dd4f2dc792c61f96eefac6871e1de
Luis Merino <mail@luismerino.name>
2023-04-24 17:14:54 +0200

    improve a couple of things in example

--- examples/node-js-basic/index.js
+++ examples/node-js-basic/index.js
@@ -1,92 +1,82 @@
-import { QdrantClient } from '@qdrant/js-client-rest';
-import assert from 'assert';
+import {QdrantClient} from '@qdrant/js-client-rest';
 
 async function main() {
-    let collectionName = 'test_collection';
+    const collectionName = 'test_collection';
 
-    let client = new QdrantClient({ url: 'http://127.0.0.1:6333' });
+    const client = new QdrantClient({url: 'http://127.0.0.1:6333'});
 
-    let response = await client.getCollections();
+    const response = await client.getCollections();
 
-    let collectionNames = response.collections.map((collection) => collection.name);
+    const collectionNames = response.collections.map((collection) => collection.name);
 
     if (collectionNames.includes(collectionName)) {
         await client.deleteCollection(collectionName);
     }
 
-    await client.createCollection(
-        collectionName,
-        {
-            vectors: {
-                size: 4,
-                distance: 'Cosine'
-            },
-            optimizers_config: {
-                default_segment_number: 2,
-            },
-            replication_factor: 2,
-        }
-    );
-
+    await client.createCollection(collectionName, {
+        vectors: {
+            size: 4,
+            distance: 'Cosine',
+        },
+        optimizers_config: {
+            default_segment_number: 2,
+        },
+        replication_factor: 2,
+    });
 
     //  -------- Create payload indexes -------------
 
     await client.createPayloadIndex(collectionName, {
-        field_name: "city",
-        field_schema: "keyword",
-        wait: true
-    });
-
-
-    await client.createPayloadIndex(collectionName, {
-        field_name: "count",
-        field_schema: "integer",
-        wait: true
+        field_name: 'city',
+        field_schema: 'keyword',
+        wait: true,
     });
 
     await client.createPayloadIndex(collectionName, {
-        field_name: "coords",
-        field_schema: "geo",
-        wait: true
+        field_name: 'count',
+        field_schema: 'integer',
+        wait: true,
+    });
+
+    await client.createPayloadIndex(collectionName, {
+        field_name: 'coords',
+        field_schema: 'geo',
+        wait: true,
     });
 
     //  -------- Add points -------------
 
-    await client.upsert(
-        collectionName,
-        {
-            wait: true,
-            points: [
-                {
-                    "id": 1,
-                    "vector": [0.05, 0.61, 0.76, 0.74],
-                    "payload": {
-                        "city": "Berlin",
-                        "country": "Germany",
-                        "count": 1000000,
-                        "square": 12.5,
-                        "coords": { "lat": 1.0, "lon": 2.0 }
-                    }
+    await client.upsert(collectionName, {
+        wait: true,
+        points: [
+            {
+                id: 1,
+                vector: [0.05, 0.61, 0.76, 0.74],
+                payload: {
+                    city: 'Berlin',
+                    country: 'Germany',
+                    count: 1000000,
+                    square: 12.5,
+                    coords: {lat: 1.0, lon: 2.0},
                 },
-                { "id": 2, "vector": [0.19, 0.81, 0.75, 0.11], "payload": { "city": ["Berlin", "London"] } },
-                { "id": 3, "vector": [0.36, 0.55, 0.47, 0.94], "payload": { "city": ["Berlin", "Moscow"] } },
-                { "id": 4, "vector": [0.18, 0.01, 0.85, 0.80], "payload": { "city": ["London", "Moscow"] } },
-                { "id": "98a9a4b1-4ef2-46fb-8315-a97d874fe1d7", "vector": [0.24, 0.18, 0.22, 0.44], "payload": { "count": [0] } },
-                { "id": "f0e09527-b096-42a8-94e9-ea94d342b925", "vector": [0.35, 0.08, 0.11, 0.44] }
-            ]
-        }
-    );
-
-    let collectionInfo = await client.getCollection(collectionName);
-    console.log(collectionInfo.points_count)
-    // prints: 6
-
-    let points = await client.retrieve(collectionName, {
-        ids: [1, 2]
+            },
+            {id: 2, vector: [0.19, 0.81, 0.75, 0.11], payload: {city: ['Berlin', 'London']}},
+            {id: 3, vector: [0.36, 0.55, 0.47, 0.94], payload: {city: ['Berlin', 'Moscow']}},
+            {id: 4, vector: [0.18, 0.01, 0.85, 0.8], payload: {city: ['London', 'Moscow']}},
+            {id: '98a9a4b1-4ef2-46fb-8315-a97d874fe1d7', vector: [0.24, 0.18, 0.22, 0.44], payload: {count: [0]}},
+            {id: 'f0e09527-b096-42a8-94e9-ea94d342b925', vector: [0.35, 0.08, 0.11, 0.44]},
+        ],
     });
 
+    const collectionInfo = await client.getCollection(collectionName);
+    console.log(collectionInfo.points_count);
+    // prints: 6
 
-    console.log("points: ", points);
+    const points = await client.retrieve(collectionName, {
+        ids: [1, 2],
+    });
+
+    console.log('points: ', points);
     // prints:
     // points:  [
     //     {
@@ -103,18 +93,15 @@ async function main() {
     //     { id: 2, payload: { city: [Array] }, vector: null }
     //   ]
 
-
-
     // -------- Search ----------------
-    let query_vector = [0.2, 0.1, 0.9, 0.7];
+    const queryVector = [0.2, 0.1, 0.9, 0.7];
 
-
-    let res1 = await client.search(collectionName, {
-        vector: query_vector,
+    const res1 = await client.search(collectionName, {
+        vector: queryVector,
         limit: 3,
     });
 
-    console.log("search result: ", res1);
+    console.log('search result: ', res1);
     // prints:
     // search result:  [
     // {
@@ -146,23 +133,20 @@ async function main() {
     // }
     // ]
 
-
-
-    let resBatch = await client.searchBatch(collectionName, {
+    const resBatch = await client.searchBatch(collectionName, {
         searches: [
             {
-                vector: query_vector,
+                vector: queryVector,
                 limit: 1,
             },
             {
-                vector: query_vector,
+                vector: queryVector,
                 limit: 2,
-            }
-        ]
+            },
+        ],
     });
 
-    console.log("search batch result: ", resBatch);
-
+    console.log('search batch result: ', resBatch);
     // prints:
     // search batch result:  [
     //     [
@@ -192,26 +176,24 @@ async function main() {
     //     ]
     // ]
 
-
-
     // -------- Search filters ----------------
 
-    let res2 = await client.search(collectionName, {
-        vector: query_vector,
+    const res2 = await client.search(collectionName, {
+        vector: queryVector,
         limit: 3,
         filter: {
             must: [
                 {
-                    "key": "city",
-                    "match": {
-                        "value": "Berlin"
-                    }
-                }
-            ]
-        }
+                    key: 'city',
+                    match: {
+                        value: 'Berlin',
+                    },
+                },
+            ],
+        },
     });
 
-    console.log("search result with filter: ", res2);
+    console.log('search result with filter: ', res2);
     // prints:
     // search result with filter:  [
     //     {
@@ -242,11 +224,17 @@ async function main() {
     //       vector: null
     //     }
     // ]
+
+    return 0;
 }
 
 main()
-    .then(() => {
-        console.log('done');
-    }).catch((err) => {
+    .then((code) => {
+        if (code !== 0) {
+            process.exit(code);
+        }
+    })
+    .catch((err) => {
         console.error(err);
+        process.exit(1);
     });
