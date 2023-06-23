48e46a945f066bee54482aa327630c1076671aac
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-06-23 16:16:44 +0000

    Revert "version up"
    This reverts commit f36ad67aa2a5fc9d96d20169e0fce19509b2b696.

--- packages/qdrant-js/scripts/integration-tests.sh
+++ packages/qdrant-js/scripts/integration-tests.sh
@@ -8,7 +8,7 @@ function stop_docker()
   docker stop qdrant_test
 }
 
-QDRANT_LATEST="v1.3.0"
+QDRANT_LATEST="v1.2.2"
 QDRANT_VERSION=${QDRANT_VERSION:-"$QDRANT_LATEST"}
 
 QDRANT_HOST='127.0.0.1:6333'
