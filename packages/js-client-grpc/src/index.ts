a00f857159037e8ba8de0e9f699b6389a0ade63b
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2023-09-05 14:43:52 +0000

    update grpc client api

--- packages/js-client-grpc/src/index.ts
+++ packages/js-client-grpc/src/index.ts
@@ -7,3 +7,4 @@ export * from './proto/json_with_int_pb.js';
 export * from './proto/points_pb.js';
 export * from './proto/qdrant_pb.js';
 export * from './proto/snapshots_service_pb.js';
+export * from './proto/qdrant_internal_service_pb.js';
