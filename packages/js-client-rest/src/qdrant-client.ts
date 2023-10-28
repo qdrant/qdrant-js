6cff0c9d18c3b0b77c1dad4847f00b6b1fe51010
Mohamed Morad <mohamed.morad@syncore.at>
2023-10-28 19:36:38 +0200

    renamed recommend_batch to recommendBatch for consistency and kept alias

--- packages/js-client-rest/src/qdrant-client.ts
+++ packages/js-client-rest/src/qdrant-client.ts
@@ -246,7 +246,7 @@ export class QdrantClient {
      *             - 'all' - query all replicas, and return values present in all replicas
      * @returns List of recommend responses
      */
-    async recommend_batch(
+    async recommendBatch(
         collection_name: string,
         {searches, consistency}: SchemaFor<'RecommendRequestBatch'> & {consistency?: SchemaFor<'ReadConsistency'>},
     ) {
@@ -257,6 +257,11 @@ export class QdrantClient {
         });
         return maybe(response.data.result).orElse([]);
     }
+
+    /**
+     * @alias recommendBatch
+     */
+    recommend_batch = this.recommendBatch;
 
     /**
      * Recommendation request. Provides positive and negative examples of the vectors,
