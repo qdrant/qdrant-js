5cc13430ded1a52d7b28d8237ec19467209fd1b7
Luis Cossío <luis.cossio@outlook.com>
2025-11-12 16:42:11 -0600

    adapt VectorOutput extensions into helpers

--- packages/js-client-grpc/src/index.ts
+++ packages/js-client-grpc/src/index.ts
@@ -1,6 +1,3 @@
-// Apply proto extensions before re-exporting (must be first)
-import './vector-output-extensions.js';
-
 export {QdrantClient, QdrantClientParams} from './qdrant-client.js';
 export * from './errors.js';
 export {ConnectError, Code as ConnectErrorCode} from '@connectrpc/connect';
@@ -9,3 +6,6 @@ export * from './proto/json_with_int_pb.js';
 export * from './proto/points_pb.js';
 export * from './proto/qdrant_pb.js';
 export * from './proto/snapshots_service_pb.js';
+
+// Helper functions
+export * from './vector-output-helpers.js';
