a1d89e6e9607e7770818d0d5ae6c3547922c303c
Luis Cossío <luis.cossio@outlook.com>
2025-11-14 09:49:20 -0600

    export common_pb.ts

--- packages/js-client-grpc/src/index.ts
+++ packages/js-client-grpc/src/index.ts
@@ -5,6 +5,7 @@ export * from './proto/collections_pb.js';
 export * from './proto/json_with_int_pb.js';
 export * from './proto/points_pb.js';
 export * from './proto/qdrant_pb.js';
+export * from './proto/common_pb.js';
 export * from './proto/snapshots_service_pb.js';
 
 // Helper functions
